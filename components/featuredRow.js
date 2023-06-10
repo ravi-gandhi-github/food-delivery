3aadd607bb6fd91f70d989c23f7bddbfab05759c
ravi-gandhi-github <gandhifamily.ravi@gmail.com>
2023-06-10 13:33:30 +0530

    Homescreen category

---
+++ components/featuredRow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function FeaturedRow() {
+  return (
+    <View>
+      <Text>FeaturedRow</Text>
+    </View>
+  );
+}
