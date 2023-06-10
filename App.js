b62d2ff095f46f82c012a7a07eee4f205b3b36c8
ravi-gandhi-github <gandhifamily.ravi@gmail.com>
2023-06-10 11:44:15 +0530

    fix

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Food Delivery App</Text>
+      <Text>Food Delivery App</Text>
       <StatusBar style="auto" />
     </View>
   );
